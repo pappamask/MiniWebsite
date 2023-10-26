33cf0c65ee547837470246cad32d63e679eeda3d
pappamask <troels.stoklund.larsen@gmail.com>
2023-10-26 19:32:03 +0200

    update

---
+++ portfolio/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Images
+
+def home(request):
+    context = {}
+    images = Images.objects.all()
+    context["images"] = images
+    return render(request, "index.html", context)
